a85547689ce0e4c5ced60b09b321545610c44b0e
Mohamed Aziz Belfaidi <azizbelfaidi.aziz@gmail.com>
2025-11-29 19:07:58 +0100

    env redirect url

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,12 +37,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      
-         
+       
  {children}
           
-       
-       
       </body>
     </html>
     </ClerkProvider>
